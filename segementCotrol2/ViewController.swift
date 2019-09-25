1c4ccb3c0b45fc12838e2d06780e72d749c255ad
R Shantha Kumar <shanth923@achiever-104.local>
2019-09-25 13:54:44 +0530

    Initial Commit

---
+++ segementCotrol2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segementCotrol2
+//
+//  Created by R Shantha Kumar on 9/25/19.
+//  Copyright © 2019 R Shantha Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ segementCotrol2Tests/segementCotrol2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  segementCotrol2Tests.swift
+//  segementCotrol2Tests
+//
+//  Created by R Shantha Kumar on 9/25/19.
+//  Copyright © 2019 R Shantha Kumar. All rights reserved.
+//
+
+import XCTest
+@testable import segementCotrol2
+
+class segementCotrol2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
